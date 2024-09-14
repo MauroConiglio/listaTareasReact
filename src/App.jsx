e4d873a81711f0f621ac65ac70a23123a961720e
Mauro Coniglio <mauroconiglioo48@gmail.com>
2024-09-14 11:44:48 -0300

    agregue formulario de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FormularioTareas from './components/FormularioTareas';
 
 function App() {
 
   return (
     <>
+    <main className='container my-5'>
 
+      <h1 className='text-center'>Bienvenidos</h1>
+      <h2>Ingresa tus tareas</h2>
+    <FormularioTareas></FormularioTareas>
+    </main>
+  
     </>
   )
 }
